4bc111f758f3644775ed01e90fb7983c4f5b4641
Jonas Liljegren <jonas@paranormal.se>
2005-08-08 09:18:04 +0000

    Renamed file interest_update.pm, formerly member_interest_update.pm

---
+++ lib/Para/Action/interest_update.pm
@@ -0,0 +1,47 @@
+#  $Id$  -*-perl-*-
+package Para::Action::member_interest_update;
+
+use strict;
+
+use Para::Frame::Utils qw( throw store_params clear_params
+			   restore_params );
+
+use Para::Member;
+
+sub handler
+{
+    my( $req ) = @_;
+
+    my @fields = ();
+    my @values = ();
+
+
+    my $q = $req->q;
+    my $u = $req->s->u;
+
+    my $tid = $q->param('tid')
+	or throw('incomplete', "tid param missing\n");
+    my $mid = $q->param('mid') || $u->id;
+    my $m = Para::Member->get($mid);
+    my $t = Para::Topic->get($tid);
+
+    if( $u->level < 40 and $u->id != $mid )
+    {
+	throw('denied', "Du har inte access för att ändra någon annans intressen.");
+    }
+
+    warn "  Will now update interest in $tid for $mid\n";
+
+    my $i = Para::Interest->getset( $m, $t );
+
+    $q->param('interest_defined') ||= 10;
+
+    if( $i->update_by_query )
+    {
+	return sprintf( "Nya uppgifter om intresset %s sparat för %s.\n",
+			$t->desig, $m->desig);
+    }
+    return "";
+}
+
+1;

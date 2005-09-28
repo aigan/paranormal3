5590c80ab624e57557b0b55b7194fe6e60642e7e
Jonas Liljegren <jonas@paranormal.se>
2005-09-28 21:59:33 +0000

    use Para::Topic->create_entry

--- lib/Para/Action/entry_create.pm
+++ lib/Para/Action/entry_create.pm
@@ -5,6 +5,7 @@ use strict;
 use Data::Dumper;
 
 use Para::Frame::Utils qw( throw trim );
+use Para::Utils qw( trim_text );
 
 use Para::Topic;
 
@@ -20,25 +21,22 @@ sub handler
 	throw('denied', "Du måste bli medborgare för att få skapa nya texter");
     }
 
-    my $text = $q->param('t_text')
+    my $new_text = $q->param('t_text')
 	or throw('incomplete', "t_text param missing");
 
     my $parent = $q->param('tid')
 	or throw('incomplete', "tid param missing");
 
+    trim_text( \$new_text );
 
-    my $tid = $Para::dbix->get_nextval( "t_seq" );
-    my $sth = $Para::dbh->prepare(
-	  "insert into t ( t, t_text, t_created, t_updated, t_createdby,
-                           t_changedby, t_status, t_active, t_entry,
-                           t_entry_parent, t_entry_imported )
-           values ( ?, ?, now(), now(), ?, ?, ?, 't', 't', ?, 1)");
-    $sth->execute( $tid, $text, $u->id, $u->id, $u->new_status, $parent ) or die;
+    return "" unless length( $new_text );
 
+    my $changes = $req->{'changes'} ||= Para::Change->new;
 
-    $u->score_change('entry_submitted', 1);
+    my $t = Para::Topic->create_entry($parent, \$new_text);
 
-    $q->param('tid',$tid);
+    $req->{'changes'}->note("Ändringen kommer att kontrolleras\n");
+
     return "Ny text skapad";
 }
 

837a2f70a808272a94ed15adf70fbb18e1637275
Jonas Liljegren <jonas@paranormal.se>
2005-09-29 08:01:12 +0000

    moved to use move_branch() and move_node() in Para::Topic

--- lib/Para/Action/outline_update.pm
+++ lib/Para/Action/outline_update.pm
@@ -19,8 +19,6 @@ our $mod;            # s or a
 # mod s = place as separate child to parent on row
 # mod a = place after row on level (default)
 
-sub dest; # (defind later) - The destination topic obj
-
 sub handler
 {
     my( $req ) = @_;
@@ -42,6 +40,7 @@ sub handler
     my @clear_fields = ();
 #    $Para::safety = 0;
 
+    my $crits = Para::Widget::tfilter_init;
 
     ############################################
     # what to do?
@@ -173,187 +172,41 @@ sub handler
     #
     foreach my $eid ( reverse $q->param("thing") )
     {
-	next if dest->id == $eid;
+	my $d = $place_parent ? $place_parent : $place_previous;
+	my $before_child = 1;
+	$before_child = 0 if $mod eq 's';
+
+	next if $d->id == $eid;
 	my $e = Para::Topic->get_by_id( $eid );
 
 	if( $do eq 'move_branch' )
 	{
-	    $result .= move_branch( $e );
+	    $result .= $e->move_branch({
+		'dest' => $d,
+		'follows' => $place_previous,
+		'before_child' => $before_child,
+		'crits' => $crits,
+	    });
 	}
 	if( $do eq 'move_node' )
 	{
-	    $result .= move_node( $e );
+	    $result .= $e->move_node({
+		'dest' => $d,
+		'follows' => $place_previous,
+		'before_child' => $before_child,
+		'crits' => $crits,
+	    });
 	}
+
+	$e->generate_url;
     }
 
 
-    push @clear_fields, 'thing';
-
-
-    clear_params( @clear_fields );
-
-    Para::Topic->commit; # Save changes
+    clear_params('thing');
 
     warn "Report:\n$result\n";
 
     return $result;
-}
-
-sub move_branch
-{
-    my( $e ) = @_;
-
-#    confess if $Para::safety++ > 1000;
-
-    my $eid = $e->id;
-    my $did = dest->id; # Destination topic id
-    my $result = "";
-
-    if( dest->child_of( $e ) )
-    {
-	return "Kan inte flytta grenen $eid till en del av sig själv\n";
-    }
-
-    if( $place_previous )
-    {
-	$result .= "* move $eid -> after $did\n";
-	debug(1,"move $eid -> after $did");
-
-	# If dest already has a "next", move that "next" to the end of
-	# the string of $e
-	if( my $old_next = dest->next )
-	{
-	    $result .= "  move $old_next->{t} after $eid\n";
-	    $result .= dest->set_next(undef);
-	    $result .= $e->last_entry->set_next($old_next);
-	}
-
-	$result .= dest->set_next($e);
-
-	# We now transform childs of dest to childs of e, if e are
-	# placed above the childs in the matrix
-	#
-	# Going through all the c below the dest in the matrix
-	#
-	for( my $i = $row_selected+1; $i <= $row_max; $i++ )
-	{
-	    my $c = $placing->{$i}{'entry'};
-	    if( my $cp = $c->parent )
-	    {
-		my $cid = $c->id;
-		my $cpid = $cp->id;
-		next unless $cpid == dest->id;
-
-		# We found a c that has dest as parent
-		#
-		$result .= "  Placing $cid under $eid\n";
-		debug(1,"  Placing $cid under $eid");
-		$result .= $c->set_parent( $e );
-	    }
-	}
-    }
-
-    if( $place_parent )
-    {
-	$result .= "* move $eid -> child of $did\n";
-	debug(1,"move $eid -> child of $did");
-
-	# decoupeling before movement
-	$result .= $e->set_parent( undef );
-	if( my $e_previous = $e->previous )
-	{
-	    $result .= $e_previous->set_next( undef );
-	}
-
-	# If there are an entry just below dest in the matrix, that
-	# has the dest as a parent; place that entry as following e,
-	# unless e should be placed separately ($mod='s')
-
-	if( my $b = $placing->{$placed+1}{'entry'} )
-	{
-	    if( $mod ne 's' and my $bp = $b->parent )
-	    {
-		my $bid = $b->id;
-		my $bpid = $bp->id;
-		if( $bpid == dest->id )
-		{
-		    debug(1,"  $bid are a child of $did");
-		    debug(1,"  place $eid before $bid");
-		    $result .= $b->set_parent(undef);
-		    $result .= $e->set_next($b);
-		}
-	    }
-	}
-
-	debug(1,"  Under: Set parent");
-	$result .= $e->set_parent( dest );
-	debug(1,"  Under: Done");
-    }
-    return $result;
-}
-
-sub move_node
-{
-    my( $e ) = @_;
-
-#    confess if $Para::safety++ > 1000;
-
-    my $eid = $e->id;
-    my $did = dest->id; # Destination topic id
-
-
-    my $result = "Kopplar loss $eid\n";
-    debug(1,"Kopplar loss $eid");
-
-    my $e_subst = undef;
-    if( $e->previous )
-    {
-	$e_subst = $e->previous;
-	$result .= $e_subst->set_next(undef );
-    }
-    elsif( $e->parent )
-    {
-	$e_subst = $e->parent;
-	$result .= $e->set_parent(undef);
-    }
-    else
-    {
-	die "No subst";
-    }
-
-    $result .= "  Flyttar barn till $e_subst->{t}\n";
-
-    # Move childs to substitute
-    my $childs = $e->childs;
-    foreach my $child ( @$childs )
-    {
-	$result .= "  make $child->{t} child to $e_subst->{t}\n";
-	$result .= $child->set_parent($e_subst);
-    }
-
-    # Move next to substitute
-    if( my $e_next = $e->next )
-    {
-	if( $e_subst->is_topic )
-	{
-	    $result .= "  make $e_next->{t} child to $e_subst->{t}\n";
-	    $result .= $e_next->set_parent($e_subst);
-	}
-	else
-	{
-	    $result .= "  make $e_next->{t} follow $e_subst->{t}\n";
-	    $result .= $e_subst->set_next( $e_next );
-	}
-	$result .= "  make $e_next->{t} no longer follow $eid\n";
-	$result .= $e->set_next( undef );
-    }
-
-    return $result . move_branch( $e );
-}
-
-sub dest
-{
-    return $place_parent ? $place_parent : $place_previous;
 }
 
 1;

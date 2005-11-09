ab29540ebeaf5b0548fa5e1059a9f4fbf23007c9
Jonas Liljegren <jonas@paranormal.se>
2005-11-09 22:38:25 +0000

    for testing

---
+++ lib/Para/Action/do_bgjob.pm
@@ -0,0 +1,17 @@
+package Para::Action::do_bgjob;
+
+use strict;
+use Sys::CpuLoad;
+
+sub handler
+{
+    my $last_time = $Para::Frame::BGJOBDATE ||= time;
+    my $delta = time - $last_time;
+    my $sysload = (Sys::CpuLoad::load)[1];
+    my $req = $Para::Frame::REQ;
+    Para::Frame::add_background_jobs($delta, $sysload);
+    Para::Frame::switch_req( $req );
+    return "BGjobs done";
+}
+
+1;

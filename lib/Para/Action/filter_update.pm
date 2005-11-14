430d45c8b5049642ea9dce58801e9a25daac475d
Jonas Liljegren <jonas@paranormal.se>
2005-11-14 00:41:17 +0000

    new action

---
+++ lib/Para/Action/filter_update.pm
@@ -0,0 +1,62 @@
+package Para::Action::filter_update;
+
+use strict;
+
+use Para::Frame::Time qw( date now );
+use Para::Frame::Utils qw( throw trim );
+
+sub handler
+{
+    my( $req ) = @_;
+
+    # Authorization
+    if( $Para::Frame::U->chat_level < 4 )
+    {
+ 	throw('denied', "Endast för ops");
+    }
+
+    my $q = $req->q;
+
+    my $pattern = $q->param('pattern');
+    my $reason  = $q->param('reason');
+    my $expire  = $q->param('expire');
+
+    if( $q->param('remove') )
+    {
+	my $sth = $Para::dbh->prepare_cached("delete from ipfilter where ipfilter_pattern=?");
+	$sth->execute( $pattern );
+	return "Filter removed\n";
+    }
+
+
+    if( length $expire )
+    {
+	$expire = date( $expire );
+	$expire or throw('validate', 'Inget bra datumformat');
+	$expire = $expire->cdate;
+    }
+    else
+    {
+	$expire = undef;
+    }
+
+    my $m = $Para::Frame::U;
+    my $updated   = now();
+    trim( \$reason );
+
+    unless( $reason )
+    {
+	throw('validate', "Ge en anledning");
+    }
+
+
+    # Store payment record
+    #
+    my $sth = $Para::dbh->prepare_cached("update ipfilter set ipfilter_reason=?, ipfilter_expire=?, ipfilter_changedby=?, ipfilter_updated=? where ipfilter_pattern=?");
+
+    $sth->execute( $reason, $expire, $m->id, $updated->cdate, $pattern );
+
+    return "Filter updated\n";
+};
+
+1;

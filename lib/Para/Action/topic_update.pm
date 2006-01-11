818108f26d651c8be97a7a62f54525e6285be6a1
Jonas Liljegren <jonas@paranormal.se>
2006-01-11 17:12:09 +0000

    setting pages relateve to home

--- lib/Para/Action/topic_update.pm
+++ lib/Para/Action/topic_update.pm
@@ -271,6 +271,7 @@ sub check_rel_create
     my $ver = $t->ver;
     my $req = $Para::Frame::REQ;
     my $q = $req->q;
+    my $site = $req->site;
 
     foreach my $row (1..5)
     {
@@ -304,7 +305,7 @@ sub check_rel_create
 		    $res->{'info'}{'alternatives'}{'replace'} = "_rel__n_${row}_rel";
 		    $res->{'info'}{'alternatives'}{'view'} = "/member/db/topic/edit/meta.tt";
 
-		    $req->set_error_template('/alternatives.tt');
+		    $req->set_error_template($site->home.'/alternatives.tt');
 		    $req->s->route->bookmark;
 		}
 		die $@; # Propagate error

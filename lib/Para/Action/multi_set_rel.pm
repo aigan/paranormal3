01fbbfcc89938f120f8d3df1fbad6910ca0cbb21
Jonas Liljegren <jonas@paranormal.se>
2006-05-19 11:12:38 +0000

    using classes and methods instead

--- lib/Para/Action/multi_set_rel.pm
+++ lib/Para/Action/multi_set_rel.pm
@@ -25,49 +25,33 @@ sub handler
 
     foreach my $key ( $q->param )
     {
-	next unless $key =~ /^relrev_(\d+)_(\d+)_(\d+)$/;
+	next unless $key =~ /^relrev_(\d+)$/;
 
-	my $reltype = $1;
-	my $rel = $2;
-	my $rev = $3;
-	my $verdict = $q->param("relrev_${reltype}_${rel}_${rev}");
+	my $id = $1;
+
+	my $verdict = $q->param("relrev_${id}");
 	next if $verdict eq '';
 
-	warn sprintf "Checking relation R$reltype($rev,$rel)\n";
+	warn sprintf "Checking relation $id\n";
 	warn " verdict $verdict\n";
 
-	my $list = $Para::dbix->select_list('from rel where rev=? and rel_type=? and rel=?
-                                and (rel_status>=? or rel_active is true)
-                                order by rel_topic', $rev, $reltype, $rel,
-			       $C_S_PROPOSED);
+	my $arc = Para::Arc->get($id);
 
-	my $active_seen = 0; # Set if any in the list is active.  Just one in the list should bee
-	foreach my $rec ( @$list ) ## In creation order
+	my $pleed = $arc->strength < $C_TRUE_MIN ? 'f' : 't';
+
+	warn sprintf "  %s\n", $arc->desig;
+	warn "  pleed $pleed\n";
+
+	if( $pleed eq $verdict )
 	{
-	    my $pleed = $rec->{'rel_strength'} < $C_TRUE_MIN
-	      ? 'f' : 't';
-
-	    my $arc = Para::Arc->get( $rec->{'rel_topic'}, $rec );
-
-	    warn sprintf "  %s\n", $arc->desig;
-	    warn "  pleed $pleed\n";
-
-
-	    if( $pleed eq $verdict )
-	    {
-		warn "  Activating this?\n";
-		if( not $active_seen )
-		{
-		    $active_seen = 1;
-		    $arc->activate;
-		}
-	    }
-	    else
-	    {
-		$arc->deactivate;
-	    }
-	    $points ++;
+	    $arc->activate;
 	}
+	else
+	{
+	    $arc->deactivate;
+	}
+
+	$points ++;
     }
 
     return "$points relationer modererade!\n";

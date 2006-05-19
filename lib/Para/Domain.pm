56bcd18c07bd2d670821cc31d41c6529d2716765
Jonas Liljegren <jonas@paranormal.se>
2006-05-19 08:57:02 +0000

    new module

---
+++ lib/Para/Domain.pm
@@ -0,0 +1,310 @@
+#  $Id$  -*-perl-*-
+package Para::Domain;
+#=====================================================================
+#
+# DESCRIPTION
+#   Paranormal.se Domain
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2006 Jonas Liljegren.  All Rights Reserved.
+#
+#   This module is free software; you can redistribute it and/or
+#   modify it under the same terms as Perl itself.
+#
+#=====================================================================
+
+use strict;
+use Data::Dumper;
+use Carp qw( confess );
+
+BEGIN
+{
+    our $VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+    print "Loading ".__PACKAGE__." $VERSION\n";
+}
+
+use Para::Frame::Reload;
+use Para::Frame::Utils qw( throw paraframe_dbm_open debug );
+
+use Para::Domains qw( :all );
+use Para::Email::Redirect;
+
+
+### INITIALIZE
+#
+our @DOMAIN_TYPE  =
+    ("Undefined",
+     "Reserverad",
+     "Normal",
+     "Namndomän",
+     );
+
+sub new
+{
+    my( $this, $name, $type, $in_dbm ) = @_;
+    my $class = ref($this) || $this;
+
+    $in_dbm ||= 0;
+
+    my $rec;
+    if( ref $type )
+    {
+	$rec = $type;
+	$rec->{in_rdb} = 1;
+	$rec->{in_dbm} = $in_dbm;
+    }
+    else
+    {
+	$rec = $Para::dbix->select_possible_record("from domain where domain=?",$name);
+
+	if( $rec )
+	{
+	    if( $type and $type != $rec->{'domain_type'} )
+	    {
+		debug "Domain $name type mismatch ($rec->{domain_type} != $type)";
+	    }
+
+	    $rec->{in_rdb} = 1;
+	    $rec->{in_dbm} = $in_dbm;
+	}
+	else
+	{
+	    $rec =
+	    {
+		in_rdb => 0,
+		in_dbm => 1,
+		domain => $name,
+		domain_type => $type,
+	    };
+	}
+    }
+
+    return bless $rec, $class;
+}
+
+sub get
+{
+    die "deprecated";
+    my( $this, $id ) = @_;
+    my $class = ref($this) || $this;
+
+    my $db = paraframe_dbm_open( DB_DOMAINS );
+    my $name = $id;
+    my $type = $db->{$id} or throw("not found", "Can't find domain $id");
+    return $this->new( $name, $type );
+}
+
+sub create
+{
+    die "not implemented";
+
+    my( $this, $name, $type ) = @_;
+
+    my $class = ref($this) || $this;
+    my $db = paraframe_dbm_open( DB_DOMAINS );
+
+    $db->{ $name } = $type;
+    return $this->new( $name, $type );
+}
+
+#########################################
+
+sub id
+{
+    my( $d ) = @_;
+    return $d->{'domain'};
+}
+
+sub name
+{
+    my( $d ) = @_;
+    return $d->{'domain'};
+}
+
+sub type
+{
+    my( $d ) = @_;
+    return $d->{'domain_type'};
+}
+
+sub type_name
+{
+    my( $d ) = @_;
+
+    return $DOMAIN_TYPE[ $d->{'domain_type'} ];
+}
+
+sub redirects
+{
+    my( $d ) = @_;
+
+    return $d->{'redirects'} || $d->redirects_init;
+}
+
+sub redirects_init
+{
+    my( $d ) = @_;
+
+    if( $d->{'redirects'} )
+    {
+	confess "Called twice";
+    }
+
+    my $by_alias = $d->{'r_by_alias'} = {};
+    my $by_id = $d->{'r_by_id'} = {};
+
+    my $db = $d->db;
+    my @redirects;
+    foreach my $alias ( keys %$db )
+    {
+	my $dest = $db->{$alias};
+	my $r = Para::Email::Redirect->new( $d, $alias, $dest, 1 );
+	push @redirects, $r;
+	$by_alias->{$alias} = $r;
+	if( $r->id )
+	{
+	    $by_id->{$r->id} = $r;
+	}
+    }
+
+    foreach my $rec ( $Para::dbix->select_list("from mailr
+                      where mailr_domain=?", $d->name)->as_list )
+    {
+	my $alias = $rec->{'mailr_name'};
+	unless( $by_alias->{$alias} )
+	{
+	    my $r = Para::Email::Redirect->new( $d, $alias, $rec, 0 );
+	    push @redirects, $r;
+	    $by_alias->{$alias} = $r;
+	    $by_id->{$r->id} = $r;
+	}
+    }
+
+    my @sorted = sort{ lc($a->alias) cmp lc($b->alias) } @redirects;
+
+    return $d->{'redirects'} = Para::Frame::List->new(\@sorted);
+}
+
+sub redirect
+{
+    my( $d, $alias ) = @_;
+
+    debug "Looking up redirect for $alias";
+
+    $alias or throw('incomplete',"Redirect method called without alias param");
+    $d->{'redirects'} or $d->redirects_init;
+    return $d->{'r_by_alias'}{$alias} || $d->{'r_by_id'}{$alias} or
+	die "Redirection $alias not found";
+}
+
+sub db_filename
+{
+    my( $d ) = @_;
+
+    return  DB_DIR . $d->name;
+}
+
+sub db
+{
+    my( $d ) = @_;
+
+    return paraframe_dbm_open( $d->db_filename );
+}
+
+sub in_rdb
+{
+    return $_[0]->{'in_rdb'};
+}
+
+sub in_dbm
+{
+    return $_[0]->{'in_dbm'};
+}
+
+#########################################
+
+sub add_in_dbm
+{
+    my( $d ) = @_;
+
+    my $name = $d->name;
+    my $type = $d->type;
+
+    my $db = paraframe_dbm_open( DB_DOMAINS );
+
+    $db->{ $name } = $type;
+
+    $d->{'in_dbm'} = 1;
+    return 1;
+}
+
+sub add_in_rdb
+{
+    my( $d ) = @_;
+
+    my $name = $d->name;
+    my $type = $d->type;
+
+    if( $d->in_rdb )
+    {
+	return 0; # Already existing
+    }
+
+    my $uid = $Para::Frame::U->id;
+
+    $Para::dbix->insert({
+	table => 'domain',
+	rec =>
+	{
+	    domain => $name,
+	    domain_type => $type,
+	    domain_updatedby => $uid,
+	},
+    });
+
+    $d->{'in_rdb'} = 1;
+    return 1;
+}
+
+sub email_create
+{
+    die "not implemented";
+
+    my( $d, $alias, $email ) = @_;
+
+    Para::Email::Redirect->create( $d, $alias, $email );
+}
+
+sub remove
+{
+    die "not implemented";
+
+    my( $d ) = @_;
+
+    my $file = $d->db_filename;
+    unlink $file or die "Failed to remove file $file: $!\n";
+
+    my $db = paraframe_dbm_open( DB_DOMAINS );
+    my $name = $d->name;
+    delete( $db->{$name} );
+    return 1;
+}
+
+sub set_type
+{
+    die "not implemented";
+
+    my( $d, $type ) = @_;
+
+    die if $type < 1;
+    die if $type > 3;
+
+    my $db = paraframe_dbm_open( DB_DOMAINS );
+    my $name = $d->name;
+    $db->{$name} = $type;
+}
+
+1;
---
+++ lib/Para/Domains.pm
@@ -0,0 +1,123 @@
+#  $Id$  -*-perl-*-
+package Para::Domains;
+#=====================================================================
+#
+# DESCRIPTION
+#   Paranormal.se Domains
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2006 Jonas Liljegren.  All Rights Reserved.
+#
+#   This module is free software; you can redistribute it and/or
+#   modify it under the same terms as Perl itself.
+#
+#=====================================================================
+
+use strict;
+use Data::Dumper;
+use Carp qw( confess );
+
+BEGIN
+{
+    our $VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+    print "Loading ".__PACKAGE__." $VERSION\n";
+
+    # Export before pulling in more modules
+    #
+    use constant DB_DOMAINS => "$WA::APPROOT/var/domains.db";
+    use constant DB_DIR => "$WA::APPROOT/var/domains/";
+
+    use base 'Exporter';
+    use vars qw( @EXPORT_OK %EXPORT_TAGS );
+    @EXPORT_OK = ( 'DB_DOMAINS', 'DB_DIR' );
+    %EXPORT_TAGS = ('all' => ['DB_DOMAINS', 'DB_DIR']);
+}
+
+use Para::Frame::Reload;
+use Para::Frame::Utils qw( paraframe_dbm_open debug );
+
+use Para::Domain;
+
+use base 'Para::Frame::List';
+
+our $INITIATED;
+
+sub init
+{
+    my( $ds ) = @_;
+
+    if( $INITIATED )
+    {
+	confess "Catched a second initiation of the domains list";
+    }
+
+    my $db = paraframe_dbm_open( DB_DOMAINS );
+    debug "Opening domain list";
+
+    my $obj = $ds->hashref;
+    $obj->{by_id} = {};
+    $obj->{not_in_dbm} = [];
+    my $by_id   = $obj->{by_id};
+    my $not_in_dbm = $obj->{not_in_dbm};
+
+    foreach my $name ( sort keys %$db )
+    {
+	my $type = $db->{$name};
+	my $d = Para::Domain->new( $name, $type, 1 );
+	push @$ds, $d;
+	$by_id->{$name} = $d;
+    }
+
+    foreach my $rec ( $Para::dbix->select_list("from domain")->as_list )
+    {
+	my $name = $rec->{'domain'};
+	unless( $by_id->{$name} )
+	{
+	    my $d = Para::Domain->new( $name, $rec, 0 );
+	    push @$not_in_dbm, $d;
+	    push @$ds, $d;
+	    $by_id->{$name} = $d;
+	}
+    }
+
+    $INITIATED ++;
+}
+
+sub get
+{
+    my( $ds, $name ) = @_;
+    if( my $d = $ds->hashref->{'by_id'}{$name} )
+    {
+	return $d;
+    }
+    else
+    {
+	debug "Domain $name not found";
+	return undef;
+    }
+}
+
+sub types
+{
+    return \@Para::Domain::DOMAIN_TYPE;
+}
+
+sub types_as_hash
+{
+    return { map{$_=>$Para::Domain::DOMAIN_TYPE[$_]} 0..$#Para::Domain::DOMAIN_TYPE };
+}
+
+sub get_all
+{
+
+    # Reimplement this since we will usually not reset the
+    # counter. Thus we will always give all the data. Not just the
+    # rest, from where we left of previously;
+
+    return [@{$_[0]}];
+}
+
+1;
